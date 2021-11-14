8ad9591160948ac648fe555f96da838fbf105994
Anavrins <xAnavrins@users.noreply.github.com>
2021-11-13 20:39:13 -0500

    Optimize peripheral calls in rednet.run
    Currently, rednet.run will do a peripheral call (.isOpen) every time it receives a modem message on an open port, regarless if rednet.open was called beforehand.
    This changes it so that `isOpen` is run last, after being checked that the message is destined to the host, and is a valid rednet formatted message.

--- switchcraft/assets/computercraft/lua/rom/apis/rednet.lua
+++ switchcraft/assets/computercraft/lua/rom/apis/rednet.lua
@@ -350,9 +350,9 @@ function run()
         if sEvent == "modem_message" then
             -- Got a modem message, process it and add it to the rednet event queue
             local sModem, nChannel, nReplyChannel, tMessage = p1, p2, p3, p4
-            if isOpen(sModem) and (nChannel == os.getComputerID() or nChannel == CHANNEL_BROADCAST) then
+            if (nChannel == os.getComputerID() or nChannel == CHANNEL_BROADCAST) then
                 if type(tMessage) == "table" and tMessage.nMessageID and (tMessage.nMessageID == tMessage.nMessageID) then
-                    if not tReceivedMessages[tMessage.nMessageID] then
+                    if not tReceivedMessages[tMessage.nMessageID] and isOpen(sModem) then
                         tReceivedMessages[tMessage.nMessageID] = true
                         tReceivedMessageTimeouts[os.startTimer(30)] = tMessage.nMessageID
                         os.queueEvent("rednet_message", nReplyChannel, tMessage.message, tMessage.sProtocol)
